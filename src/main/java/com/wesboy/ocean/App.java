b147e299fee79dd5504920e9916a51d60c09645e
wsboy <pengbo.wang@yi23.net>
2018-02-05 11:16:47 +0800

    init

---
+++ src/main/java/com/wesboy/ocean/App.java
@@ -0,0 +1,13 @@
+package com.wesboy.ocean;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
